292eb9d570b6499f9792f61e93b82f3e50c9338e
ArmaanGIS <59824117+ArmaanGIS@users.noreply.github.com>
2023-04-11 23:01:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -41,7 +41,8 @@ if st.button("Generate Sitemap"):
                 f.write(sitemap)
 
             # Display sitemap for debugging purposes
-            st.markdown(f"Sitemap generated! Download <a href='https://armaangis-streamlit-bingsearchsitemap-app-gkp1c8.streamlit.app/{filename}' download target='_blank'>here</a>.", unsafe_allow_html=True)
+            share_url = st.get_share_streamlit_app_url()
+            st.markdown(f"Sitemap generated! Download <a href='{share_url}/{filename}' download target='_blank'>here</a>.", unsafe_allow_html=True)
 
     except requests.exceptions.RequestException as e:
         st.error(f"API request failed with error {e}")
